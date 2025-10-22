2f3f7966972514ebf25561a34840d748d6ec5f7f
jojoselix <damfpioc@gmail.com>
2025-10-22 23:03:04 +0200

    sqllite on discount.grpc

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,28 @@
+
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(opts =>
+        opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
+
+builder.Services.AddGrpcReflection();
+
+var app = builder.Build();
+
+app.UseMigration();
+app.MapGrpcService<DiscountService>();
+
+// Habilitar reflexión solo en desarrollo (buena práctica)
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
+
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
